09595e1e268dd88bbe1d64249a41a2803f80744a
TaiseiNemoto <nemoto@defide-ix.com>
2023-08-30 09:53:34 +0900

    [add]Tailwindにカラーテーマ追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,7 +7,43 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      green: {
+        one: '#E7F3EF',
+        two: '#DDEEE9',
+        three: '#C6E2D9',
+        four: '#8CC5B3',
+        five: '#39705E',
+      },
+      gray: {
+        one: '#E5E7EB',
+        two: '#D1D5DB',
+        three: '#4B5563',
+        four: '#374151',
+        five: '#111827',
+      },
+      olive: {
+        one: '#E1EAE2',
+        two: '#DAE5DB',
+        three: '#A5C0A7',
+        four: '#729C76',
+      },
+      cream: {
+        one: '#FFFFFF',
+        two: '#FEFEFB',
+        three: '#FDFCF7',
+        four: '#FCFBF4',
+      },
+      steel: {
+        one: '#FAFAFA',
+        two: '#F5F5F5',
+        three: '#737373',
+      },
+      leaf: {
+        one: '#E0E7A1',
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
