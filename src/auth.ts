56c9912ca9f1119a42742a2dceb130bc6017c029
TaiseiNemoto <nemoto@defide-ix.com>
2023-09-01 10:15:34 +0900

    [add]NextAuthのセットアップ（Githubの設定まで）

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,11 @@
-import NextAuth from 'next-auth';
+import { type NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-const handler = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
     }),
   ],
-});
-
-export { handler as GET, handler as POST };
+};
