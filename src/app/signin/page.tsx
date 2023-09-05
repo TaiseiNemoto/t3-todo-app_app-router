f12e2089547db0c25f57c9434aff04b9300c2c26
TaiseiNemoto <nemoto@defide-ix.com>
2023-09-05 10:16:50 +0900

    [WIP]signinページの作成：providersの取得まで完了

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,1 +1,30 @@
-export default function SignIn() {}
+import { getProviders } from 'next-auth/react';
+
+export default async function SignIn() {
+  const providers = await getProviders();
+  // https://nextjs.org/docs/app/building-your-application/routing/route-handlers
+
+  return (
+    <>
+      <div>
+        {providers?.github.id}
+        {/* {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button
+              className="inline-flex w-full cursor-pointer items-center justify-center rounded-md p-4 text-xl font-bold hover:text-green-five"
+              // このボタンを押すと GitHub による認証が行われます
+              // また、認証後のリダイレクト先をルートパスに設定しています
+              onClick={() =>
+                void signIn(provider.id, {
+                  callbackUrl: '/',
+                })
+              }
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))} */}
+      </div>
+    </>
+  );
+}
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,34 @@
-import { type NextAuthOptions } from 'next-auth';
+import { type GetServerSidePropsContext } from 'next';
+import {
+  getServerSession,
+  type NextAuthOptions,
+  type DefaultSession,
+} from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 import prisma from './db';
 
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession['user'];
+  }
+}
+
 export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
@@ -12,4 +36,14 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
     }),
   ],
+  pages: {
+    signIn: '/signin',
+  },
+};
+
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext['req'];
+  res: GetServerSidePropsContext['res'];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
 };
