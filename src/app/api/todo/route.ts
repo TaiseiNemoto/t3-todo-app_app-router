83512540fa020e5e93e3c8e59cc7155d70510677
TaiseiNemoto <nemoto@defide-ix.com>
2023-09-25 10:16:01 +0900

    [WIP]Todoを作成し、DBに登録する：DBにTodoが登録できるところまで確認

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
-  console.log(prisma);
+  console.log(data);
 
-  prisma.todo.create({
+  const todo = await prisma.todo.create({
     data: {
       text: data.newTodo,
       user: {
@@ -20,5 +20,5 @@ export async function POST(req: NextRequest) {
   // const note = await prisma.note.create({
   //   data: { title: parcedData.title, body: parcedData.body },
   // });
-  return new NextResponse(`${data}`, { status: 201 });
+  return new NextResponse(`${todo}`, { status: 201 });
 }
--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client';
-// Prismaはサーバーサイドでしか使用できないためserver-onlyをインポートする
-import 'server-only';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
