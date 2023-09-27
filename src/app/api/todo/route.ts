fecd2bc550fc2749eb1cf346977a6666569a225b
TaiseiNemoto <nemoto@defide-ix.com>
2023-09-27 10:14:20 +0900

    [add]Todoコンポーネントの作成：Todo削除APIまで作成

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,7 +1,8 @@
-import { prisma } from '@/db';
+import { z } from 'zod';
 
+import { prisma } from '@/db';
 import { NextRequest, NextResponse } from 'next/server';
-import { createInput } from './type';
+import { createInput, updateInput } from './type';
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
@@ -19,4 +20,28 @@ export async function POST(req: NextRequest) {
   });
 
   return new NextResponse(`${todo.id}`, { status: 201 });
+}
+
+export async function PUT(req: NextRequest) {
+  const data = await req.json();
+  const parcedData = updateInput.parse(data);
+  const { id, text, is_completed } = parcedData;
+  await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      isCompleted: is_completed,
+      text: text,
+    },
+  });
+  return new NextResponse(null, { status: 204 });
+}
+
+export async function DELETE(req: NextRequest) {
+  const parcedData = z.string().parse(req);
+  await prisma.todo.delete({
+    where: { id: parcedData },
+  });
+  return new NextResponse(null, { status: 204 });
 }
--- src/app/api/todo/type.ts
+++ src/app/api/todo/type.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod';
 
+const zTodo = z.object({
+  id: z.string(),
+  text: z.string(),
+  isCompleted: z.boolean(),
+});
+
 export const createInput = z.object({
   newTodo: z
     .string()
@@ -7,3 +13,19 @@ export const createInput = z.object({
     .max(50, 'todo must be 50 letters or less'),
   userId: z.string(),
 });
+
+export const updateInput = z.object({
+  id: z.string(),
+  text: z
+    .string()
+    .min(1, 'todo must be at least 1 letter')
+    .max(50, 'todo must be 50 letters or less'),
+  is_completed: z.boolean(),
+});
+
+export const toggleInput = z.object({
+  id: z.string(),
+  is_completed: z.boolean(),
+});
+
+export type Todo = z.infer<typeof zTodo>;
